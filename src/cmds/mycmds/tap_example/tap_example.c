224cb2ec50e0015970dc95395b7693b94c7757bf
kamendov-maxim <kamendov.maksim@icloud.com>
2025-04-27 18:39:57 +0300

    tun and tap drivers: in progress

--- src/cmds/mycmds/tap_example/tap_example.c
+++ src/cmds/mycmds/tap_example/tap_example.c
@@ -5,12 +5,12 @@
 #include <unistd.h>
 #include <net/if.h>
 
-int create_tun_device(char *dev_name, int flag) {
+int create_tap_device(char *dev_name, int flag) {
 	struct ifreq ifr;
 	int fd, err_code;
 
-	if ((fd = open("/dev/tun0", O_RDWR)) < 0) {
-		printf("Tun device fd creation error. (%d)\n", fd);
+	if ((fd = open("/dev/tap0", O_RDWR)) < 0) {
+		printf("Tap device fd creation error. (%d)\n", fd);
 		return fd;
 	}
 
@@ -19,7 +19,7 @@ int create_tun_device(char *dev_name, int flag) {
 	ifr.ifr_flags |= flag;
 
 	if ((err_code = ioctl(fd, FIONBIO, &ifr)) < 0) {
-		printf("Tun device ioctl error. (%d)\n", err_code);
+		printf("Tap device ioctl error. (%d)\n", err_code);
 		close(fd);
 		return err_code;
 	}
@@ -28,35 +28,35 @@ int create_tun_device(char *dev_name, int flag) {
 }
 
 int main(int argc, char *argv[]) {
-	int tun_fd = create_tun_device("tun0", 0x0002);
-	if (tun_fd < 0) {
-		printf("tun_fd < 0\n");
+	int tap_fd = create_tap_device("tap0", 0x0002);
+	if (tap_fd < 0) {
+		printf("tap_fd < 0\n");
 	}
-	printf("Tun device created successfully\n");
+	printf("Tap device created successfully\n");
 
 	int ret_length = 0;
 	unsigned char buf[1024];
 	while (1) {
-		ret_length = read(tun_fd, buf, sizeof(buf));
+		ret_length = read(tap_fd, buf, sizeof(buf));
 		if (ret_length < 0) {
 			break;
 		}
 		unsigned char src_ip[4];
 		unsigned char dst_ip[4];
-		memcpy(src_ip, &buf[12], 4);
-		memcpy(dst_ip, &buf[16], 4);
+		memcpy(src_ip, &buf[26], 4);
+		memcpy(dst_ip, &buf[30], 4);
 		printf("ICMP receive : %d.%d.%d.%d -> %d.%d.%d.%d (%d)\n", dst_ip[0],
 		    dst_ip[1], dst_ip[2], dst_ip[3], src_ip[0], src_ip[1], src_ip[2],
 		    src_ip[3], ret_length);
 
-		memcpy(&buf[12], dst_ip, 4);
-		memcpy(&buf[16], src_ip, 4);
+		memcpy(&buf[26], dst_ip, 4);
+		memcpy(&buf[30], src_ip, 4);
 		buf[24] = 0;
-		ret_length = write(tun_fd, buf, ret_length);
+		ret_length = write(tap_fd, buf, ret_length);
 		printf("ICMP send : %d.%d.%d.%d -> %d.%d.%d.%d (%d)\n", src_ip[0],
 		    src_ip[1], src_ip[2], src_ip[3], dst_ip[0], dst_ip[1], dst_ip[2],
 		    dst_ip[3], ret_length);
 	}
 
-	return close(tun_fd);
+	return close(tap_fd);
 }
