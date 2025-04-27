ebd2d22867c99000bd95c13bca172c909a2a5a93
kamendov-maxim <kamendov.maksim@icloud.com>
2025-04-27 18:39:57 +0300

    tun and tap drivers: in progress

--- src/drivers/net/tun/1tun.c
+++ src/drivers/net/tun/1tun.c
@@ -24,8 +24,6 @@
 #include <net/l0/net_entry.h>
 #include <net/l2/ethernet.h>
 #include <net/l3/arp.h>
-#include <net/l3/ipv4/ip_options.h>
-
 #include <net/netdevice.h>
 #include <util/err.h>
 #include <util/math.h>
@@ -67,13 +65,13 @@ static int tun_set_mac(struct net_device *dev, const void *addr) {
 
 static int tun_xmit(struct net_device *dev, struct sk_buff *skb) {
 	struct tun_dev *tun = netdev_priv(dev);
-	/* struct ethhdr *ethh; */
+	struct ethhdr *ethh;
 
 	/* we don't build headers for dev with NOARP flag */
-	/* ethh = eth_hdr(skb); */
-	/* ethh->h_proto = htons(ETH_P_IP); */
-	/* memcpy(ethh->h_source, skb->dev->dev_addr, ETH_ALEN); */
-	/* memset(ethh->h_dest, 0, ETH_ALEN); */
+	ethh = eth_hdr(skb);
+	ethh->h_proto = htons(ETH_P_IP);
+	memcpy(ethh->h_source, skb->dev->dev_addr, ETH_ALEN);
+	memset(ethh->h_dest, 0, ETH_ALEN);
 
 	skb_queue_push(&tun->rx_q, skb);
 	waitq_wakeup(&tun->wq, 1);
@@ -88,11 +86,8 @@ static const struct net_driver tun_netdrv_ops = {
 
 static int tun_setup(struct net_device *dev) {
 	dev->mtu = (16 * 1024) + 20 + 20 + 12;
-	/* dev->hdr_len = ETH_HEADER_SIZE; */
-	/* dev->addr_len = ETH_ALEN; */
-	dev->hdr_len = 0;
-	dev->addr_len = 0;
-	/* dev->type = ARP_HRD_LOOPBACK; */
+	dev->hdr_len = ETH_HEADER_SIZE;
+	dev->addr_len = ETH_ALEN;
 	dev->type = ARP_HRD_LOOPBACK;
 	dev->flags = IFF_NOARP | IFF_RUNNING;
 	dev->drv_ops = &tun_netdrv_ops;
@@ -113,8 +108,6 @@ static void tun_dev_close(struct char_dev *cdev) {
 
 	tun_user_unlock((struct tun_dev *)cdev);
 }
-
-#include <net/l3/ipv4/ip.h>
 
 static ssize_t tun_dev_read(struct char_dev *cdev, void *buf, size_t nbyte) {
 	struct waitq_link *wql = &thread_self()->schedee.waitq_link;
@@ -139,9 +132,7 @@ static ssize_t tun_dev_read(struct char_dev *cdev, void *buf, size_t nbyte) {
 		if (skb) {
 			min_len = min(skb->len, nbyte);
 			if (min_len > 0) {
-				/* memcpy(buf, skb->mac.raw, min_len); */
-
-				memcpy(buf, skb->nh.raw, min_len);
+				memcpy(buf, skb->mac.raw, min_len);
 				ret = min_len;
 			}
 			break;
@@ -160,31 +151,27 @@ static ssize_t tun_dev_write(struct char_dev *cdev, const void *buf,
 	struct net_device *netdev;
 	struct tun_dev *tun;
 	struct sk_buff *skb;
-	/* unsigned char *raw; */
-	/* struct ethhdr *ethh; */
+	unsigned char *raw;
+	struct ethhdr *ethh;
 
 	assert(cdev);
 
 	tun = (struct tun_dev *)cdev;
 	netdev = tun->netdev;
 
-	skb = skb_alloc(nbyte
-                 /* + ETH_HLEN */
-                 );
+	skb = skb_alloc(nbyte + ETH_HLEN);
 	if (!skb) {
 		return -ENOMEM;
 	}
 
-	/* ethh = eth_hdr(skb); */
+	ethh = eth_hdr(skb);
 
-	/* ethh->h_proto = htons(ETH_P_IP); */
-	/* memcpy(ethh->h_dest, netdev->dev_addr, ETH_ALEN); */
-	/* memset(ethh->h_source, 0, ETH_ALEN); */
+	ethh->h_proto = htons(ETH_P_IP);
+	memcpy(ethh->h_dest, netdev->dev_addr, ETH_ALEN);
+	memset(ethh->h_source, 0, ETH_ALEN);
 
-	/* raw = skb->mac.raw + ETH_HLEN; */
-	/* memcpy(raw, buf, nbyte); */
-
-    memcpy(skb->data, buf, nbyte);
+	raw = skb->mac.raw + ETH_HLEN;
+	memcpy(raw, buf, nbyte);
 
 	skb->dev = netdev;
 	netif_rx(skb);
